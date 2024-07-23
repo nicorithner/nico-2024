20a7441a3d54aaecef537d063d3101acf8edc12a
Nico Rithner <nicorithner@gmail.com>
2024-07-22 21:28:17 -0600

    clean up page to provide a starting point

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-  <h1 className="text-3xl font-bold">Nico&apos;s new portfolio</h1>
+    <main className="flex justify-center items-center">
+      <div>
+        <h1 className="text-3xl font-bold">Nico&apos;s new portfolio</h1>
+      </div>
+    </main>
   );
 }
