0b017ba0834402ecad328822360e06b173484309
Nico Rithner <nicorithner@gmail.com>
2024-07-22 20:33:43 -0600

    Add tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <h1>Nico&apos;s new portfolio</h1>
+  <h1 className="text-3xl font-bold">Nico&apos;s new portfolio</h1>
   );
 }
