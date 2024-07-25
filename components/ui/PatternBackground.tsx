aadc6f462d52833b9795b7cc6b292993e5a218c4
Nico Rithner <nicorithner@gmail.com>
2024-07-24 21:03:56 -0600

    Add PatternBackground model using aceternity

---
+++ components/ui/PatternBackground.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function PatternBackground({
+  styles,
+  gridType,
+}: {
+  styles: string;
+  gridType: string;
+}) {
+  return (
+    <div className={`${gridType} ${styles}`}>
+      {/* Radial gradient for the container to give a faded look */}
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+    </div>
+  );
+}
