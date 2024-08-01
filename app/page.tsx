c2a2984c8b488c84cdca376838e2f890aca863e6
Nico Rithner <nicorithner@gmail.com>
2024-08-01 08:47:48 -0600

    Add BentoGrid component to app page.Add css to global

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import BentoGrid from "../components/bentoGrid/BentoGrid";
 import Hero from "../components/hero/Hero";
 
 export default function Home() {
@@ -5,6 +6,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
+        <BentoGrid />
       </div>
     </main>
   );
