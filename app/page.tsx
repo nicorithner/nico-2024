6afcd08b57a746b3043fb5964477050a846bce38
Nico Rithner <nicorithner@gmail.com>
2024-08-01 17:56:10 -0600

    Add techStack component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import BentoGrid from "../components/bentoGrid/BentoGrid";
 import Hero from "../components/hero/Hero";
+import TechStackCard from "../components/techStack/TechStack";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        <BentoGrid />
+        <TechStackCard />
       </div>
     </main>
   );
