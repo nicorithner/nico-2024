cad24d4e197953759d4e59a323e0eb7aed1ca156
Nico Rithner <nicorithner@gmail.com>
2024-08-16 18:10:08 -0600

    Update Experience. Style large screen
    Mobile view needs work

--- components/workExperience/WorkExperience.tsx
+++ components/workExperience/WorkExperience.tsx
@@ -5,9 +5,9 @@ import { experience } from "../../data";
 export default function WorkExperience() {
   const currentJob = experience[0];
   return (
-    <div className="relative py-20 w-full">
+    <div id="experience" className="relative py-20 w-full">
       <h1 className="heading">
-        My <span className="text-purple">work experience</span>
+        My <span className="text-purple">Experience</span>
       </h1>
 
       <div className="w-full mt-12 grid lg:grid-cols-4 grid-cols-1 gap-10">
@@ -23,32 +23,102 @@ export default function WorkExperience() {
           }}
           className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
         >
-          <div className="flex flex-col p-3 py-6 md:p-5 lg:p-10 gap-4">
+          <div className="flex flex-col p-3 py-6 md:p-5 lg:p-10 gap-4 w-full">
             <img
               src={currentJob.thumbnail}
               alt={currentJob.thumbnail}
-              className="lg:w-full md:w-auto w-16"
+              className="w-full lg:w-auto w-16"
             />
             <div className="lg:ms-5 text-left space-y-4">
-              <h1 className="text-start lg:text-4xl font-bold">
+              <h1 className="text-start text-xl lg:text-4xl font-bold">
                 {currentJob.company}
               </h1>
-              <h2 className="text-start text-lg md:text-xl font-bold">
+              <h2 className="text-start text-base lg:text-xl font-bold">
                 {currentJob.title}
               </h2>
+              <h3 className="text-left italic text-sm lg:text-base">
+                {`${currentJob.location} - ${currentJob.years[0]} to ${currentJob.years[1]}`}
+              </h3>
               <ul className="list-none text-left">
                 {currentJob.desc.map((str, index): React.ReactNode => {
-                    return <li key={index + 1}>{str}</li>;
+                  return <li key={index + 1}>{str}</li>;
                 })}
               </ul>
               <div className="flex gap-2 flex-wrap mt-2">
                 {currentJob.stack.map((str, index): React.ReactNode => {
-                    return <span className="text-yellow-200 font-semibold" key={index + 1}>{str}</span>;
+                  return (
+                    <span
+                      className="text-yellow-200 font-semibold"
+                      key={index + 1}
+                    >
+                      {str}
+                    </span>
+                  );
                 })}
               </div>
             </div>
           </div>
         </MovingBorderCard>
+        <div className="grid gap-2 col-span-2 px-2">
+          {experience.map((job, index): React.ReactNode => {
+            return (
+              job.years[1] !== "current" && (
+                <MovingBorderCard
+                  key={job.id}
+                  duration={Math.floor(Math.random() * 10000) + 10000}
+                  borderRadius="1.75rem"
+                  style={{
+                    background: "rgb(4,7,29)",
+                    backgroundColor:
+                      "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+                    borderRadius: `calc(1.75rem* 0.96)`,
+                  }}
+                  className="flex-1 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                >
+                  <div className="flex lg:flex-row flex-col lg:items-center p-2 py-3 md:p-2 lg:p-5 gap-1">
+                    <img
+                      src={job.thumbnail}
+                      alt={job.thumbnail}
+                      className="lg:w-32 md:w-20 w-16"
+                    />
+                    <div className="lg:ms-5">
+                      <h1 className="text-start text-xl lg:text-2xl font-bold">
+                        {job.company}
+                      </h1>
+                      <h2 className="text-start text-base lg:text-lg font-bold">
+                        {job.title}
+                      </h2>
+                      <h3 className="text-left lg:text-sm italic">
+                        {`${job.location} - ${job.years[0]} to ${job.years[1]}`}
+                      </h3>
+                      <ul className="mt-1 list-none text-left text-xs">
+                        {job.desc.map((str, index): React.ReactNode => {
+                          return (
+                            <li className="mb-2" key={index + 1}>
+                              {str}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      <div className="flex gap-2 flex-wrap mt-2 text-xs">
+                        {job.stack.map((str, index): React.ReactNode => {
+                          return (
+                            <span
+                              className="text-yellow-200 font-semibold"
+                              key={index + 1}
+                            >
+                              {str}
+                            </span>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                </MovingBorderCard>
+              )
+            );
+          })}
+        </div>
       </div>
     </div>
   );
